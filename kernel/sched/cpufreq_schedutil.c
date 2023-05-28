24e063e82ab8ce6664bba6d023a388dccb8c7cbb
rooghz720 <RooGhz720@gmail.com>
2023-05-29 01:22:25 +0800

    cpufreq: schedutil: Expose default hispeed load
    Signed-off-by: rooghz720 <RooGhz720@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -389,7 +389,6 @@ static inline bool sugov_cpu_is_busy(struct sugov_cpu *sg_cpu) { return false; }
 #endif /* CONFIG_NO_HZ_COMMON */
 
 #define NL_RATIO 75
-#define DEFAULT_HISPEED_LOAD 90
 static void sugov_walt_adjust(struct sugov_cpu *sg_cpu, unsigned long *util,
 			      unsigned long *max)
 {
@@ -993,7 +992,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	  break;
 	}
 	
-	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
+	tunables->hispeed_load = CONFIG_SCHEDUTIL_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 
 	policy->governor_data = sg_policy;
