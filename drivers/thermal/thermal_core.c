90536e22e2541cbc7c40d7735233d66fb9d60baa
RooGhz720 <RooGhz720@gmail.com>
2023-06-09 19:48:15 +0800

    thermal: add simple thermal lock mechanism
    Some ROMs (mainly MIUI) can override user/kernel specific thermal selection
    so add a small thermal feature to lock a specific selection. When enabled,
    kernel will use and lock specified thermal selection system wide.
    When disabled, kernel will follow default ROM/kernel behavior.
    
    Note: this is used by my NoGravity Kernel App to lock specific POCO
    thermal profiles while leaving the choice to user to run default ROM configuration
     when not on a dynamic thermal ROM like MIUI.
    
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -82,6 +82,11 @@ static struct thermal_governor *def_governor;
 
 static struct workqueue_struct *thermal_passive_wq;
 
+static int lock_enable = 0;
+static int lock_sconfig = -1;
+module_param(lock_enable, int, 0644);
+module_param(lock_sconfig, int, 0644);
+
 /*
  * Governor section: set of functions to handle thermal governors
  *
@@ -1711,7 +1716,10 @@ thermal_sconfig_store(struct device *dev,
 
 	ret = kstrtoint(buf, 10, &val);
 
-	atomic_set(&switch_mode, val);
+	if(lock_enable)
+		atomic_set(&switch_mode, lock_sconfig);
+	else
+		atomic_set(&switch_mode, val);
 
 	if (ret)
 		return ret;
