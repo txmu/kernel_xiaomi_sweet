49adc1d1784e0fee21f1a1c836ec20d0a682cab4
RooGhz720 <RooGhz720@gmail.com>
2023-06-15 17:07:39 +0800

    cpu_input_boost: break cpu input boost when kprofile 1
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -18,6 +18,8 @@ static unsigned int input_boost_freq_lp = CONFIG_INPUT_BOOST_FREQ_LP;
 static unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
 static unsigned short input_boost_duration = CONFIG_INPUT_BOOST_DURATION_MS;
 
+extern int kp_active_mode(void);
+
 module_param(input_boost_freq_lp, uint, 0644);
 module_param(input_boost_freq_hp, uint, 0644);
 module_param(input_boost_duration, short, 0644);
