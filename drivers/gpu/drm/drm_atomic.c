98df64ad915f12f22ef9d1edde101be15391c355
RooGhz720 <RooGhz720@gmail.com>
2023-06-16 15:47:17 +0800

    drm: fix missing devfreq_boost defined
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -34,7 +34,7 @@
 #include <linux/devfreq_boost.h>
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
-
+#include <linux/devfreq_boost.h>
 #include "drm_crtc_internal.h"
 
 void __drm_crtc_commit_free(struct kref *kref)
